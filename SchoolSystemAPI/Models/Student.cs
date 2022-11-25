c1bed8ed853a421cf528aa4084397927b117bd30
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-25 14:50:25 -0600

    finished my model for the student

--- SchoolSystemAPI/Models/Student.cs
+++ SchoolSystemAPI/Models/Student.cs
@@ -1,6 +1,18 @@
-﻿namespace SchoolSystemAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolSystemAPI.Models
 {
     public class Student
     {
+        [Key]
+        [Required]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string StudentEmail { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = null!; 
+        public IEnumerable<Subject> Subjects { get; set; } = Enumerable.Empty<Subject>(); 
+
+
     }
 }
