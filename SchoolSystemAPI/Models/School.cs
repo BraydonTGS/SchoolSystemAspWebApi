b1ceda0674a7655e3c9faf347810c651f644ce44
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-25 14:45:55 -0600

    finished my school model

--- SchoolSystemAPI/Models/School.cs
+++ SchoolSystemAPI/Models/School.cs
@@ -1,6 +1,19 @@
-﻿namespace SchoolSystemAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolSystemAPI.Models
 {
     public class School
     {
+        [Key]
+        [Required]
+        public int SchoolId { get; set; }
+        public string SchoolName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string PostalCode { get; set; } = null!; 
+
+        public IEnumerable<Teacher> Teachers { get; set; } = Enumerable.Empty<Teacher>(); 
+        public IEnumerable<Student> Students { get; set; } = Enumerable.Empty<Student>(); 
     }
 }
