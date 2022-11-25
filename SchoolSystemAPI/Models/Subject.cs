0c1340648e10974d4b941876921935e6668da8f6
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-25 14:52:50 -0600

    added my subject model

--- SchoolSystemAPI/Models/Subject.cs
+++ SchoolSystemAPI/Models/Subject.cs
@@ -1,6 +1,14 @@
-﻿namespace SchoolSystemAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolSystemAPI.Models
 {
     public class Subject
     {
+        [Key]
+        [Required]
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; } = null!; 
+       public ClassRoom ClassRoom { get; set; } 
+
     }
 }
