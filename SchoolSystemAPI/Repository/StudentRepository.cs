28a8baa68f16cd1e023e6d0c61f782f2d7595c5c
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-28 19:18:56 -0600

    implemented my async methods for my student repository

--- SchoolSystemAPI/Repository/StudentRepository.cs
+++ SchoolSystemAPI/Repository/StudentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SchoolSystemAPI.Data;
 using SchoolSystemAPI.Interfaces;
+using SchoolSystemAPI.Models;
 
 namespace SchoolSystemAPI.Repository
 {
@@ -11,6 +12,54 @@ namespace SchoolSystemAPI.Repository
         public StudentRepository(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        // Create A Student //
+        public async Task<Student> CreateStudentAsync(Student student)
+        {
+            await _context.Students.AddAsync(student);
+            await _context.SaveChangesAsync();
+            return student;
+        }
+
+        // Delete a Student //
+        public void DeleteStudent(Student student)
+        {
+            _context.Remove(student);
+            _context.SaveChanges();
+        }
+
+        // Get a List of Students //
+        public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+        {
+            var student = await _context.Students.ToListAsync();
+            return student;
+        }
+        
+        // Get Student By Id //
+        public async Task<Student?> GetStudentByIdAsync(int Id)
+        {
+            return await _context.Students.Where(s => s.StudentId == Id).FirstOrDefaultAsync();
+        }
+
+        // Does Student Exist //
+        public async Task<bool> StudentExistsAsync(int Id)
+        {
+            return await _context.Students.AnyAsync(s => s.StudentId == Id);
+        }
+
+        // Update Student //
+        public async Task<Student> UpdateStudentAsync(Student student, Student newStudent)
+        {
+            student.StudentId = newStudent.StudentId;
+            student.FirstName = newStudent.FirstName;
+            student.LastName = newStudent.LastName;
+            student.Email = newStudent.Email;
+            student.ContactNumber = newStudent.ContactNumber;
+
+            _context.Students.Attach(student);
+            await _context.SaveChangesAsync();
+            return student;
         }
     }
 }
