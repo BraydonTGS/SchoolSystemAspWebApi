557b08e26b795816e15166fd5b50d8908fca59fc
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-30 11:49:48 -0600

    set up my injections for my TeacherRepository and my TeacherController

---
+++ SchoolSystemAPI/Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolSystemAPI.Interfaces;
+
+namespace SchoolSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly ITeacherRepository _repository;
+
+        public TeacherController(ITeacherRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
--- SchoolSystemAPI/Repository/TeacherRepository.cs
+++ SchoolSystemAPI/Repository/TeacherRepository.cs
@@ -1,8 +1,15 @@
-﻿using SchoolSystemAPI.Interfaces;
+﻿using SchoolSystemAPI.Data;
+using SchoolSystemAPI.Interfaces;
 
 namespace SchoolSystemAPI.Repository
 {
     public class TeacherRepository : ITeacherRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public TeacherRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
