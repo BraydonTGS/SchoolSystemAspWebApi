b1e69521b0e018b85528f7062a572663b1e3461d
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-29 20:07:58 -0600

    started to implement my subject controller, set up my subject dto

--- SchoolSystemAPI/Controllers/SubjectController.cs
+++ SchoolSystemAPI/Controllers/SubjectController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolSystemAPI.DTO;
 using SchoolSystemAPI.Interfaces;
+using SchoolSystemAPI.Models;
 
 namespace SchoolSystemAPI.Controllers
 {
@@ -13,6 +15,25 @@ namespace SchoolSystemAPI.Controllers
         public SubjectController(ISubjectRepository repository)
         {
             _repository = repository;
+        }
+
+        // Get all Subjects //
+        [Route("GetAllSubjects")]
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Subject>))]
+        public async Task<IActionResult> GetStudents()
+        {
+            var subjects = await _repository.GetAllSubjectsAsync();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var subjectDTO = subjects.Select(x => new SubjectDTO(x.SubjectId, x.SubjectName, x.ClassRoom)); 
+
+
+            return Ok(subjectDTO);
         }
     }
 }
--- SchoolSystemAPI/DTO/SubjectDTO.cs
+++ SchoolSystemAPI/DTO/SubjectDTO.cs
@@ -5,10 +5,18 @@ namespace SchoolSystemAPI.DTO
 {
     public class SubjectDTO
     {
+   
         [Key]
         [Required]
         public int SubjectId { get; set; }
         public string SubjectName { get; set; } = null!;
         public ClassRoom ClassRoom { get; set; } = null!;
+
+        public SubjectDTO(int subjectId, string subjectName, ClassRoom classRoom)
+        {
+            SubjectId = subjectId;
+            SubjectName = subjectName;
+            ClassRoom = classRoom;
+        }
     }
 }
