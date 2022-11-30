fef65e7e9716c0f982701af03de3a7cd1ec40a75
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-29 18:42:13 -0600

    starting to build out the subject repository

--- SchoolSystemAPI/Interfaces/ISubjectRepository.cs
+++ SchoolSystemAPI/Interfaces/ISubjectRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace SchoolSystemAPI.Interfaces
+﻿using SchoolSystemAPI.Models;
+
+namespace SchoolSystemAPI.Interfaces
 {
     public interface ISubjectRepository
     {
+        public Task<IEnumerable<Subject>> GetAllSubjectsAsync();
+        public Task<Subject?> GetSubjectByIdAsync(int Id);
+
+        public Task<Subject> CreateSubjectAsync(Subject subject);
+        public Task<bool> SubjectExistsAsync(int Id);
+
+        public Task<Subject> UpdateSubjectAsync(Subject subject, Subject newSubject);
+
+        public void DeleteStudent(Subject subject);
     }
 }
--- SchoolSystemAPI/Repository/SubjectRepository.cs
+++ SchoolSystemAPI/Repository/SubjectRepository.cs
@@ -1,5 +1,7 @@
-﻿using SchoolSystemAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystemAPI.Data;
 using SchoolSystemAPI.Interfaces;
+using SchoolSystemAPI.Models;
 
 namespace SchoolSystemAPI.Repository
 {
@@ -10,6 +12,38 @@ namespace SchoolSystemAPI.Repository
         public SubjectRepository(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        public Task<Subject> CreateSubjectAsync(Subject subject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStudent(Subject subject)
+        {
+            _context.Remove(subject);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Subject>> GetAllSubjectsAsync()
+        {
+            var subjects = await _context.Subjects.ToListAsync();
+            return subjects;
+        }
+
+        public async Task<Subject?> GetSubjectByIdAsync(int Id)
+        {
+            return await _context.Subjects.Where(s => s.SubjectId == Id).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> SubjectExistsAsync(int Id)
+        {
+            return await _context.Subjects.AnyAsync(s => s.SubjectId == Id);
+        }
+
+        public Task<Subject> UpdateSubjectAsync(Subject subject, Subject newSubject)
+        {
+            throw new NotImplementedException();
         }
     }
 }
