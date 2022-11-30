3014501f694747f64583e442af9580745dcc0d40
Braydon Sutherland <braydonsutherland@gmail.com>
2022-11-30 12:05:42 -0600

    Implemented the methods on the TeacherRepository

--- SchoolSystemAPI/Interfaces/ITeacherRepository.cs
+++ SchoolSystemAPI/Interfaces/ITeacherRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace SchoolSystemAPI.Interfaces
+﻿using SchoolSystemAPI.Models;
+
+namespace SchoolSystemAPI.Interfaces
 {
     public interface ITeacherRepository
     {
+        public Task<IEnumerable<Teacher>> GetAllTeachersAsync();
+        public Task<Teacher?> GetTeacherByIdAsync(int Id);
+
+        public Task<Teacher> CreateTeacherAsync(Teacher teacher);
+        public Task<bool> TeacherExistsAsync(int Id);
+
+        public Task<Teacher> UpdateTeacherAsync(Teacher teacher, Teacher newTeacher);
+
+        public void DeleteTeacher(Teacher teacher);
     }
 }
--- SchoolSystemAPI/Repository/TeacherRepository.cs
+++ SchoolSystemAPI/Repository/TeacherRepository.cs
@@ -1,5 +1,7 @@
-﻿using SchoolSystemAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystemAPI.Data;
 using SchoolSystemAPI.Interfaces;
+using SchoolSystemAPI.Models;
 
 namespace SchoolSystemAPI.Repository
 {
@@ -11,5 +13,52 @@ namespace SchoolSystemAPI.Repository
         {
             _context = context;
         }
+
+        public async Task<Teacher> CreateTeacherAsync(Teacher teacher)
+        {
+            await _context.Teachers.AddAsync(teacher);
+            await _context.SaveChangesAsync();
+            return teacher;
+        }
+
+        public void DeleteTeacher(Teacher teacher)
+        {
+            _context.Remove(teacher);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Teacher>> GetAllTeachersAsync()
+        {
+            var teacher = await _context.Teachers.ToListAsync();
+            return teacher;
+        }
+
+        public async Task<Teacher?> GetTeacherByIdAsync(int Id)
+        {
+            return await _context.Teachers.Where(t => t.TeacherId == Id).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> TeacherExistsAsync(int Id)
+        {
+            return await _context.Teachers.AnyAsync(t => t.TeacherId == Id);
+        }
+
+
+        public async Task<Teacher> UpdateTeacherAsync(Teacher teacher, Teacher newTeacher)
+        {
+
+            teacher.FirstName= newTeacher.FirstName;
+            teacher.LastName = newTeacher.LastName;
+            teacher.ContactNumber = newTeacher.ContactNumber;
+            teacher.Email= newTeacher.Email;
+
+            _context.Teachers.Attach(teacher);
+            await _context.SaveChangesAsync();
+            return teacher;
+        }
     }
 }
+
+    
+    
+
